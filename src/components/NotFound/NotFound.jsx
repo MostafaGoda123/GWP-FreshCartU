81765682795d3b8df9d0d7e3ed32a5301f378a31
Eng\ Mostafa El-Sayed <104257870+MostafaGoda123@users.noreply.github.com>
2024-05-04 11:31:39 +0300

    Delete src directory

--- src/components/NotFound/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './NotFound.css'
-
-export default function NotFound() {
-   return <>
-      <h1>NotFound</h1>
-   </>
-}
--- src/components/Products/Products.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Products.css'
-import FeaturedProducts from '../FeaturedProducts/FeaturedProducts'
-
-export default function Products() {
-   return <>
-      <FeaturedProducts />
-   </>
-}
